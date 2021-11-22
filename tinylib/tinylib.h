35e91ec83226e1ecbe72ac64af208ebea12d2d85
David Haworth <dh@thelancashireman.org>
2021-11-22 20:02:32 +0100

    Add signed data types

--- tinylib/tinylib.h
+++ tinylib/tinylib.h
@@ -54,6 +54,11 @@ typedef unsigned short u16_t;
 typedef unsigned long u32_t;
 typedef unsigned long long u64_t;
 
+typedef signed char s8_t;
+typedef signed short s16_t;
+typedef signed long s32_t;
+typedef signed long long s64_t;
+
 // Pin modes and states
 #define LOW			0
 #define HIGH		1
