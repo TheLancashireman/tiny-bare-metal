2c0cc386f9ca2134c4722dcca3dd8a2a150efcc7
David Haworth <dh@thelancashireman.org>
2023-01-15 07:06:38 +0100

    Corrections to the GPL comments

--- tiny1w/tiny1w.c
+++ tiny1w/tiny1w.c
@@ -2,20 +2,20 @@
  *
  * (c) David Haworth
  *
- *  This file is part of one-wire.
+ *  This file is part of tiny-bare-metal.
  *
- *  one-wire is free software: you can redistribute it and/or modify
+ *  tiny-bare-metal is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  one-wire is distributed in the hope that it will be useful,
+ *  tiny-bare-metal is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with one-wire.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with tiny-bare-metal.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "tiny1w.h"
 
