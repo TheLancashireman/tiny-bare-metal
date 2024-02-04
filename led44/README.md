4d9ed5489006f386c4b6ba055a3e13cd026b98d1
David Haworth <dh@thelancashireman.org>
2024-02-04 17:32:38 +0100

    Initial version of AtTiny44 LED blinker

---
+++ led44/README.md
@@ -0,0 +1,29 @@
+# led44
+
+LED blink program on attiny44 bare metal - 20ms on, once every 2 seconds.
+
+The timing in this sketch is done using timing loops.
+
+The aim is to get the AtTiny44 running with an external crystal and/or external clock.
+
+No arduino libraries were harmed during the construction of this application. :-)
+
+## License, disclaimer etc.
+
+Copyright David Haworth
+
+This file is part of tiny-bare-metal.
+
+tiny-bare-metal is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+tiny-bare-metal is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with tiny-bare-metal.  If not, see <http://www.gnu.org/licenses/>.
+
---
+++ led44/led44.c
@@ -0,0 +1,54 @@
+/* led85.c - led blinking application
+ *
+ * (c) David Haworth
+ *
+ *  This file is part of tiny-bare-metal.
+ *
+ *  tiny-bare-metal is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  tiny-bare-metal is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with tiny-bare-metal.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "tinylib.h"
+
+#define ledpin  PB1
+
+void delay(unsigned ms);
+
+int main(void)
+{
+	pin_mode(ledpin, OUTPUT);
+
+	for (;;)
+	{
+		pin_set(ledpin, HIGH);
+		delay(20);
+		pin_set(ledpin, LOW);
+		delay(1980);
+	}
+
+	return 0;
+}
+
+void delay(unsigned ms)
+{
+	register unsigned t;
+	while ( ms > 0 )
+	{
+		t = (((unsigned long)HZ)*3)/16000;
+		while ( t > 0 )
+		{
+			__asm__ __volatile__ ("nop");
+			t--;
+		}
+		ms--;
+	}
+}
